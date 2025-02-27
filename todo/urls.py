00930962cd8fad36ed7d338b7ca9087822b87ca4
Just_NoWorking322 <alisherbolotbekov03@gmail.com>
2025-02-27 17:44:39 +0600

    urls + views

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, TodoViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"todos", TodoViewSet, basename="todo")  # Добавляем таски
 
 urlpatterns = [
     path("", include(router.urls)),  
--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,29 @@
 from rest_framework import viewsets, permissions
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        """Для создания пользователей не требуется авторизация"""
+        if self.action == "create":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
 
     def get_queryset(self):
+        """Пользователь видит только свой профиль"""
         return User.objects.filter(id=self.request.user.id)
 
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        """Эндпоинт для получения данных о себе"""
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+    
 '''Базовый Api класс чтобы создавать пользователей'''
